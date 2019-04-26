6a77ccb9620fbd8e6914e7f6f422f3497949cb5b
Alex Kyriakidis <hootlex@icloud.com>
2019-04-26 14:46:53 +0200

    seeds changes

--- seeds.js
+++ seeds.js
@@ -11,7 +11,7 @@ export default {
     'doctor',
     'nurse'
   ],
-  pages: ['create', 'update', 'index', '_id'], // we need to have 50 per module, add textX for rest pages | 500 page components so far
+  pages: ['A0', 'A1'], // we need to have 50 per module, add textX for rest pages | 500 page components so far
   components: [
     // 10 per page
     'UserCreateForm',
